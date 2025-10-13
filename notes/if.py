fbb4f6f8fabb9105f797da5fc79446aa85beb5c1
limasjg <limasjg@outlook.com>
2025-10-13 07:45:51 -0300

    New notes

---
+++ notes/if.py
@@ -0,0 +1,33 @@
+# If do some code only if some condition is True, Else do something else.
+
+#First example:
+# age = int(input("Enter your age: "))
+
+# if age >= 100:
+#     print("You are too old to sign up.")
+# if age >= 18 and age <= 100:
+#     print("You are now signed up!")
+# elif age <= 0:
+#     print("You haven't been born yet.")
+# else:
+#     print("You must be 18+ to sign up or less then 100 years.")
+
+
+# Second example:
+# name = input("Whats you name? ")
+
+# if name == "":
+#     print("You didn't type your name.")
+# else:
+#     print(f"Hello {name}")
+
+#Third example:
+online = False
+
+if online:
+    print("You are online.")
+else:
+    print("You are offline.")
+
+
+
