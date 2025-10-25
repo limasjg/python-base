a1e80201477c9058f9565d487bfb137b7db0024c
limasjg <limasjg@outlook.com>
2025-10-25 09:35:59 -0300

    Strings notes

---
+++ notes/conditional-expression.py
@@ -0,0 +1,14 @@
+# A one-line shortcut for if-else statement, print or assign one of two values based on a condition
+# formula: X IF condition ELSE Y 
+
+num = 4
+a = 15
+b = 9
+age = 1
+temp = 28
+#print("Positive" if num > 0 else "Negative" )
+# print("Even" if num % 2 == 0 else "Odd")
+# print(a if a < b else b)
+# print("Adult" if age >= 18 else "Not Adult")
+
+print("HOT" if temp >= 28 else "Nice")
---
+++ notes/strings.py
@@ -0,0 +1,17 @@
+# In python we have many built-in functions with strings
+
+# full_name = input("Enter your full name: ")
+phone_number = input("Enter your phone number: ")
+
+# result = len(full_name)
+# result = full_name.find("o")
+# result = full_name.rfind("o")
+# result = full_name.capitalize()
+# result = full_name.upper()
+# result = full_name.lower()
+# result = phone_number.isdigit()
+# result = full_name.isalpha()
+# result = phone_number.count("-")
+result = phone_number.replace("-", " ")
+
+print(result)
