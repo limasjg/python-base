1bca7370488141d5ebd20250854c94363ac34c26
limasjg <limasjg@outlook.com>
2025-11-02 11:43:15 -0300

    Collections

---
+++ notes/collections-notes.py
@@ -0,0 +1,26 @@
+#Collection is a single variable with many values stored
+# list = [] ordered, changeable, duplicates ok.
+# set = {} unordered, unchangeable, but add/remove ok, no duplicates
+# tuple = () ordered and unchangeable, duplicate ok, faster.
+
+
+# List
+animes = ["Naruto", "Dragon Ball", "Hunter x Hunter", "One punchman"]
+
+# print(animes[::2]) # You can indexing the values
+#print(dir(animes))
+# print(help(animes))
+#print(len(animes))
+#print("Naruto" in animes)
+# animes[0] = "One Piece"
+# animes.append("One Piece")
+# animes.remove("One punchman")
+# animes.sort()
+# animes.reverse()
+# animes.clear()
+# print(animes.index("Naruto"))
+print(animes.count("Naruto"))
+print(animes)
+
+# for anime in animes:
+#     print(anime)
