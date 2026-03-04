88b3a132f5c55a3f6a8114e9072d1b7853ff35f2
limasjg <limasjg@outlook.com>
2026-03-04 16:35:35 -0300

    Back to Game!

---
+++ notes/note_random.py
@@ -0,0 +1,15 @@
+import random
+
+low = 1
+high = 100
+
+options = ("Rock", "Paper", "Scissors")
+# number = random.randint(low, high)
+# print(number)
+option = random.choice(options)
+print(option)
+
+
+
+
+
--- projects/stand.py
+++ projects/stand.py
@@ -16,3 +16,18 @@ print("------ MENU ------")
 for key, value in menu.items():
     print(f"{key:10}: ${value:.2f}")
 print("------------------")
+
+while True:
+    food = input("Select an item (q to quit): ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+for food in cart:
+    total += menu.get(food)
+    print(food, end=" ")
+
+print()
+print("------ YOUR ORDER ------")
+print(f"The total is: ${total:.2f}")
