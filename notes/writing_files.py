ed976dd31d6758367aff6d2e9a2530d17394ae7c
limasjg <limasjg@outlook.com>
2026-04-14 08:18:06 -0300

    New note of working with files in python

--- notes/writing_files.py
+++ notes/writing_files.py
@@ -1,9 +1,74 @@
 # Python writing files (.txt, .json, .csv)
 
-txt_data = "Hello, this is a sample text file.\nThis file is used to demonstrate writing files in Python."
 
-file_path = r"C:\Users\limas\Desktop\example.txt"
+##################################.TXT FILES ###############################
+# txt_data = "Hello, this is a sample text file.\nThis file is used to demonstrate writing files in Python."
 
-with open(file_path, 'w') as file:
-    file.write(txt_data)
-    print(f"Data has been written to {file_path}")
+# file_path = r"C:\Users\limas\Desktop\example.txt"
+
+# try:
+#     with open(file_path, 'a') as file:
+#         file.write("\n" + txt_data)
+#         print(f"the file in {file_path} was created")
+
+# except FileExistsError:
+#         print(f"The file {file_path} already exists.")
+
+# fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+
+# file_path = r"C:\Users\limas\Desktop\example.txt"
+
+# try:
+#     with open(file_path, 'w') as file:
+#         for fruit in fruits:
+#             file.write("\n" + fruit)
+#         print(f"the file in {file_path} was created")
+
+# except FileExistsError:
+#         print(f"The file {file_path} already exists.")
+
+
+
+
+##################################.JSON FILES ###############################
+
+# import json
+
+# employees = {
+#     "employees": [
+#         {"name": "Alice", "age": 30, "department": "HR"},
+#         {"name": "Bob", "age": 25, "department": "Engineering"},
+#         {"name": "Charlie", "age": 35, "department": "Sales"}
+#     ]
+# }
+
+# file_path = r"C:\Users\limas\Desktop\example.json"
+
+# try:
+#     with open(file_path, 'w') as file:
+#         json.dump(employees, file, indent=4)
+#         print(f"the file in {file_path} was created")
+
+# except FileExistsError:
+#         print(f"The file {file_path} already exists.")
+
+##################################.CSV FILES ###############################
+
+import csv
+
+employees = [["Name", "Age", "Department"],
+             ["Alice", 30, "HR"],
+             ["Bob", 25, "Engineering"],
+             ["Charlie", 35, "Sales"]]
+
+file_path = r"C:\Users\limas\Desktop\example.csv"
+
+try:
+    with open(file_path, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';') 
+        writer.writerows(employees)
+        print(f"the file {file_path} was created")
+
+except Exception as e:
+    print(f"Error: {e}")
+
