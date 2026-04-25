369dee737ddf4266ceaad0e6371bcc36e35e71dc
limasjg <limasjg@outlook.com>
2026-04-25 17:52:02 -0300

    Add radio buttons and checkbox GUI examples

---
+++ notes/gui-checkbox.py
@@ -0,0 +1,34 @@
+# Checkbox
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)  # x, y, width, height
+        self.checkbox = QCheckBox("Check me", self)
+        self.initUI()
+
+    def initUI(self):
+        self.checkbox.setGeometry(10, 0, 500, 100)
+        self.checkbox.setStyleSheet("font-size: 30px;"
+                                    "font-family: Arial;")
+        self.checkbox.setChecked(False)  # Set the checkbox to be checked by default
+        self.checkbox.stateChanged.connect(self.checkbox_changed)
+
+    def checkbox_changed(self, state):
+        if state == Qt.Checked:
+            print("Checkbox state checked")
+        else:
+            print("Checkbox state unchecked")
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
